dad3adb6cf7fe244ee4e47d7a49974385259d3b5
kevinsunil <kevinsunil.2001@gmail.com>
2022-02-15 23:58:37 +0530

    fifth commit

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,30 +2,31 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import { Rating } from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles';
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
 const Map = () => {
-    const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width: 600px)');
-
-    const coordinates = {lat:0, lng: 0};
-    return(
-        <div className={classes.mapContainer}>
-            <GoogleMapReact 
-                    bootstrapURLKeys={{key: 'AIzaSyDFr96IRy2Y2w-l0i3wmHxh7Z_YeQaDsl0'}}
-                    defaultCenter={coordinates}
-                    center={coordinates}
-                    margin={[50, 50, 50, 50]}
-                    options={''}
-                    onChange={''}
-                    onChildClick={''}
-            >
-
-            </GoogleMapReact>
-        </div>
-    );
-}
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const coords = {lat: 0, lng: 0};
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDFr96IRy2Y2w-l0i3wmHxh7Z_YeQaDsl0' }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+        
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
